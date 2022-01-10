dc059608f0a170e12894ab971bfd7c6a8b70a326
Jaredare <drakelongtoot@gmail.com>
2022-01-10 15:19:11 -0600

    Made the main portion of the tictactoe program.

---
+++ TicTacToe.py
@@ -0,0 +1,170 @@
+import math
+
+def main():
+
+    run_game = True
+    turn = 0
+    
+    grid_size = int(input(f"How big of a grid would you like to play tic tac toe on? "))
+    spacing = round(math.log(grid_size, 10) + 0.5)
+    game_state = create_initial_state(grid_size)
+
+    while run_game == True:
+        game_state, turn, run_game, winner = game_step(game_state, turn, grid_size, spacing)
+
+    print(f"Congratulations to player {winner} for winning!")
+
+
+
+def game_step(game_state, turn, grid_size, spacing):
+    """This function will move the game along by one turn."""
+    screen_print(game_state, spacing)
+
+    game_state, turn, win_combo = do_move(game_state, turn, grid_size)
+
+    run_game, winner = check_for_winner(win_combo) 
+
+    
+
+    return game_state, turn, run_game, winner
+
+    
+
+def screen_print(game_state, spacing):
+    """Prints the screen of the game so the player can visualize it."""
+    for i in game_state:
+        for j in range(len(i)):
+
+            if j + 1 != len(i):
+                # Incorporate this. {:>5d}
+                # {:>`spacing`d}
+                printed_digit = i[j]
+                if isinstance(printed_digit, int):
+                    printed_digit += 1
+                print(f" {printed_digit} |", end="")
+
+            elif j + 1 == len(i):
+                printed_digit = i[j]
+                if isinstance(printed_digit, int):
+                    printed_digit += 1
+                print(f" {printed_digit}")
+    
+
+
+def create_initial_state(grid_size):
+    """Creates the initial state of the game for the rest of the game to work off of."""
+    # I want to change `number` to start at 1, and `choice` to -1 later.
+    number = 0
+    game_state = []
+
+    for _ in range(grid_size):
+        row = []
+        for _ in range(grid_size):
+            row.append(number)
+            number += 1
+        game_state.append(row)
+    
+    return game_state
+
+
+
+def do_move(game_state, turn, grid_size):
+    """Takes user input to turn a number in to an X or O depending on who's turn it is."""
+    # Is the turn X's or O's?
+    if turn == 0:
+        turn = 1
+        player = "X"
+    elif turn == 1:
+        turn = 0
+        player = "O"
+
+
+    Valid_Choice = False
+    while Valid_Choice == False:
+
+        user_choice = (int(input(f"{player}, where would you like to go?")) - 1)
+        row_choice = game_state[user_choice // grid_size]
+
+        if row_choice[user_choice % grid_size] != "X" and row_choice[user_choice % grid_size] != "O":
+            row_choice[user_choice % grid_size] = player
+            Valid_Choice = True
+
+        else:
+            print(f"Number {user_choice} has an {row_choice[user_choice % grid_size]} in it. Please choose another.")
+        
+    return game_state, turn, (user_choice, game_state, player, grid_size)
+
+
+
+def check_for_winner(combo):
+    """Checks to see if the active player's move just now was a winning one."""
+    user_choice, game_state, player, grid_size = combo
+
+    row_choice = game_state[user_choice // grid_size]
+    
+    # Horizontal
+
+    # Choice plus two to the right.
+    if row_choice[user_choice % grid_size] == player and row_choice[(user_choice % grid_size) + 1] == player and row_choice[(user_choice % grid_size) + 2] == player:
+        win = True
+    # Choice plus two to the left.
+    elif row_choice[user_choice % grid_size] == player and row_choice[(user_choice % grid_size) - 1] == player and row_choice[(user_choice % grid_size) - 2] == player:
+        win = True
+    # Choice plus one on either side.
+    elif row_choice[user_choice % grid_size] == player and row_choice[(user_choice % grid_size) - 1] == player and row_choice[(user_choice % grid_size) + 1] == player:
+        win = True
+
+
+    # Vertical
+
+    # Choice plus two down.
+    if (row_choice)[user_choice % grid_size] == player and (row_choice + 1)[user_choice % grid_size] == player and (row_choice + 2)[user_choice % grid_size] == player:
+        win = True
+    # Choice plus two up.
+    elif (row_choice)[user_choice % grid_size] == player and (row_choice - 1)[user_choice % grid_size] == player and (row_choice - 2)[user_choice % grid_size] == player:
+        win = True
+    # Choice plus one up and one down.
+    elif (row_choice)[user_choice % grid_size] == player and (row_choice + 1)[user_choice % grid_size] == player and (row_choice - 1)[user_choice % grid_size] == player:
+        win = True
+
+
+    # Diagonal /
+
+    # Choice plus two up-right
+    if (row_choice)[user_choice % grid_size] == player and (row_choice - 1)[(user_choice % grid_size) + 1] == player and (row_choice - 2)[user_choice % grid_size + 2] == player:
+        win = True
+    # Choice plus two down-left
+    elif (row_choice)[user_choice % grid_size] == player and (row_choice + 1)[user_choice % grid_size - 1] == player and (row_choice + 2)[user_choice % grid_size - 2] == player:
+        win = True
+    # Choice plus one up-right and one down-left.
+    elif (row_choice)[user_choice % grid_size] == player and (row_choice + 1)[user_choice % grid_size - 1] == player and (row_choice - 1)[user_choice % grid_size + 1] == player:
+        win = True
+    
+
+    # Diagonal \
+
+    # Choice plus two down-right
+    if (row_choice)[user_choice % grid_size] == player and (row_choice + 1)[(user_choice % grid_size) + 1] == player and (row_choice + 2)[user_choice % grid_size + 2] == player:
+        win = True
+    # Choice plus two up-left
+    elif (row_choice)[user_choice % grid_size] == player and (row_choice - 1)[user_choice % grid_size - 1] == player and (row_choice - 2)[user_choice % grid_size - 2] == player:
+        win = True
+    # Choice plus one down-right and one up-left.
+    elif (row_choice)[user_choice % grid_size] == player and (row_choice - 1)[user_choice % grid_size - 1] == player and (row_choice + 1)[user_choice % grid_size + 1] == player:
+        win = True
+
+
+    # Is the game won?    
+    if win:
+        run_game = False
+        winner = player
+    else: 
+        run_game = True
+        winner = "none"
+    
+    return run_game, winner
+
+
+
+if __name__ == "__main__":
+    main()
